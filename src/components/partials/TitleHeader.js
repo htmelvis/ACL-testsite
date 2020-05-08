d786599567d64e724014385033ba33e77ab1731c
htmelvis <ed@htmelvis.com>
2020-05-07 23:38:19 -0400

    scroll to top on route change

--- src/components/partials/TitleHeader.js
+++ src/components/partials/TitleHeader.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Link } from 'react-router-dom'
+import {useLocation} from "react-router";
 
-const TitleHeader = props => (
-	<div className="w-full">
-		<header className="relative w-full pb-4">
-			<div className="short-header-bg text-white object-cover w-full">
-				<div className="container px-4 md_px-0 md_w-full pt-8">
-					<p className="text-white uppercase text-xs">Associated Clinical Labs</p>
-					<h1 className=" font-bold text-3xl uppercase">{props.title}</h1>
-					<p className="text-white text-sm lg_w-2--3">
-						Northwestern Pennsylvania’s leading provider of diagnostic testing, information and services
-					</p>
+const TitleHeader = props => {
+	const { pathname } = useLocation();
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
+	return (
+		<div className="w-full">
+			<header className="relative w-full pb-4">
+				<div className="short-header-bg text-white object-cover w-full">
+					<div className="container px-4 md_px-0 md_w-full pt-8">
+						<p className="text-white uppercase text-xs">Associated Clinical Labs</p>
+						<h1 className=" font-bold text-3xl uppercase">{props.title}</h1>
+						<p className="text-white text-sm lg_w-2--3">
+							Northwestern Pennsylvania’s leading provider of diagnostic testing, information and services
+						</p>
+					</div>
 				</div>
-			</div>
-			<img src="/images/hero-wave.png" className="wave w-full absolute" alt="ACL Hero Wave" />
-		</header>
-	</div>
-);
+				<img src="/images/hero-wave.png" className="wave w-full absolute" alt="ACL Hero Wave"/>
+			</header>
+		</div>
+	)
+};
 
 export default TitleHeader;
