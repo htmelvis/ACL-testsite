442c063c5b80b431389dffc6f9c3a375fa731525
htmelvis <ed@htmelvis.com>
2020-07-13 17:54:25 -0400

    ACL navblocks fix

--- src/components/partials/Sidebar.js
+++ src/components/partials/Sidebar.js
@@ -25,7 +25,7 @@ const Sidebar = () => {
 
 				<ul>
 					<li><NavLink to="/locations">
-						Find A Locations
+						Find A Location
 					</NavLink></li>
 					<li><NavLink to="/appointments">
 						Schedule An Appointment
@@ -51,7 +51,7 @@ const Sidebar = () => {
 		</p>
 		<h4 className="text-center uppercase text-blue-300 text-xl font-bold my-4">Toll Free Telephone</h4>
 		<h3 className="text-center font-bold text-blue-100 text-2xl my-4 mb-8">1-800-937-8028</h3>
-		{!locationsMatch &&
+		{!locationsMatch && !home && !home.isExact &&
 		<Link to="/locations">
 			<img src="images/Location.png" alt="Find ACL Location" className="mx-auto" />
 		</Link>
